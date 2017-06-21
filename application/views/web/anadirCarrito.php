3c225d3e4bffbb6b1fe7bc8a3c660a4d0a503354
LuisBSC15 <luallain@hotmail.com>
2017-06-21 01:10:08 -0500

    Update anadirCarrito.php
    Corregido responsive

--- application/views/web/anadirCarrito.php
+++ application/views/web/anadirCarrito.php
@@ -1,78 +1,190 @@
-<!DOCTYPE html>
-<html>
-<script type="text/javascript" src="<?php echo base_url("public/js/jQuery-3.2.1.min.js"); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url("public/js/bootstrap.js"); ?>"></script>
-<head>
-	<meta charset="utf-8" />
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<style type="text/css">
-  		.pos {float:left;}
-	</style>
-</head>
-<body>
-	<div class='container'>
+<div class='container'>
     <div class="row">
       <div class="col-md-12">
-          <h1>Catalogo</h1>
+          <h1>Catálogo</h1>
       </div>
     </div>
     <div class="row">
-      <div class>
-	  <div class="pos" id="imagen1">
-		<img src="https://s3.amazonaws.com/poderpda/2016/09/Xperia-XZ_forestBlue.png" WIDTH=640 HEIGHT=480> 
-	  </div class="pos"
-          <div class="pos">
-            <h4>Xperia XZ</h4>
-            <div class="pos">
-                <label for="precio">Precio 
-			<input name="precio" placeholder="$600" readonly="readonly"/>
-		</label><br></br>
-                <label for="stock">Stock 
-			<input name="stock" placeholder="103" readonly="readonly"/>
-		</label><br></br>
-                <label for="caracteristicas">Caracteristicas </label><br></br>		
-                <input type="caracteristica1" name="caracteristica1" placeholder="Pantalla 5.5 Pulgadas 4K Resolucion 3840 x 2160 pixels" size="50" readonly="readonly"/><br></br>
-		<input type="caracteristica2" name="caracteristica2" placeholder="Android 7.0 Nougat" size="50" readonly="readonly"/><br></br>
-		<input type="caracteristica3" name="caracteristica3" placeholder="Procesador Qualcomm MSM8998 Snapdragon 835" size="50" readonly="readonly"/><br></br>
-		<input type="caracteristica4" name="caracteristica4" placeholder="3GB de Ram & 64 GB memoria interna" size="50" readonly="readonly"/><br></br>
-	     </div class="pos">
-		<div class="pos">
-            	<input class="btn btn-primary" name="anadir" type="submit" value="Anadir a Carrito de compras" /> </div class="pos">
-          </div>
+      <div class="container form-group">
+        <form action="<?php echo base_url('') ?>" method="POST">
+          <div class="container">
+            <div class="col-md-10 col-md-offset-1 well">
+				<div class="row">
+					<div class="col-md-2">
+						<img class="img-responsive" src="https://s3.amazonaws.com/poderpda/2016/09/Xperia-XZ_forestBlue.png" WIDTH=320 HEIGHT=240>
+					</div>
+					<div class="col-md-5">
+						<h5>Xperia XZ</h5>
+						<p>
+							Pantalla 5.5 Pulgadas 4K Resolucion 3840 x 2160 pixels
+						</p>
+						<p>
+							Android 7.0 Nougat
+						</p>
+						<p>
+							Procesador Qualcomm MSM8998 Snapdragon 835
+						</p>
+						<p>
+							3GB de Ram & 64 GB memoria interna
+						</p>
+						<a href="url"> Ver más..</a>
+					</div>
+					<div class="col-md-2">
+						<div class="row">
+							<div class="col-md-12">
+								Precio
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								$ 600
+							</div>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="row">
+							<div class="col-md-12">
+								Stock
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								134
+							</div>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="row">
+							<div class="col-md-12">
+								<input class="btn btn-primary" name="submit" type="submit" value="Añadir al Carrito de Compras" onclick="pulsar()"/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+            <div class="col-md-10 col-md-offset-1 well">
+				<div class="row">
+					<div class="col-md-2">
+						<img class="img-responsive" src="http://s7d2.scene7.com/is/image/SamsungUS/Pdpkeyfeature-sm-g935pzdaspr-600x600-C1-062016?$product-details-jpg$" WIDTH=320 HEIGHT=240>
+					</div>
+					<div class="col-md-5">
+						<h5>Samsung S7 Edge</h5>
+						<p>
+							Pantalla 5.5 Pulgadas 2K Resolucion 2440 x 1860 pixels
+						</p>
+						<p>
+							Android 7.0 Nougat
+						</p>
+						<p>
+							Procesador Exynos 8890 Octa 2.3GHz
+						</p>
+						<p>
+							4GB de Ram & 32 GB memoria interna
+						</p>
+						<a href="url"> Ver más..</a>
+					</div>
+					<div class="col-md-2">
+						<div class="row">
+							<div class="col-md-12">
+								Precio
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								$ 600
+							</div>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="row">
+							<div class="col-md-12">
+								Stock
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								65
+							</div>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="row">
+							<div class="col-md-12">
+								<input class="btn btn-primary" name="submit" type="submit" value="Añadir al Carrito de Compras" onclick="pulsar()"/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+          
+
+	  <div class="col-md-10 col-md-offset-1 well">
+				<div class="row">
+					<div class="col-md-2">
+						<img class="img-responsive" src="http://www.lg.com/es/images/gnb-global/es_gnb_he_003.png" WIDTH=320 HEIGHT=240>
+					</div>
+					<div class="col-md-5">
+						<h5>LG OLED 4K (65pulg)</h5>
+						<p>
+							MÁS NEGRO: Debido a la ausencia de retroiluminación obtendrás el único negro puro imposible de conseguir en otras tecnologías
+						</p>
+						<p>
+							MÁS COLOR: No hay colores como los de OLED. Tiene el 100% de espacio DCI-P3MÁS RÁPIDO: Es 1000 veces más rápido generando imágenes
+						</p>
+						<p>
+							MÁS ENVOLVENTE: El único TV con sonido Dolby Atmos . El sonido se mueve 360º alrededor de ti
+						</p>
+						<p>
+							MÁS SEGURO: Tiene el Smart TV más fácil, rápido y seguro. WebOS 3.5
+						</p>
+						<a href="url"> Ver más..</a>
+					</div>
+					<div class="col-md-2">
+						<div class="row">
+							<div class="col-md-12">
+								Precio
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								$ 7794
+							</div>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="row">
+							<div class="col-md-12">
+								Stock
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								34
+							</div>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="row">
+							<div class="col-md-12">
+								<input class="btn btn-primary" name="submit" type="submit" value="Añadir al Carrito de Compras" onclick="pulsar()"/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+          </div>	
+        </form>
+      </div>
+
   </div>
 </div>
-
-
-	<div class='container'>
-    <div class="row">
-    
-    </div>
-    <div class="row">
-      <div class>
-	  <div class="pos" id="imagen1">
-		<img src="http://s7d2.scene7.com/is/image/SamsungUS/Pdpkeyfeature-sm-g935pzdaspr-600x600-C1-062016?$product-details-jpg$" WIDTH=640 HEIGHT=480> 
-	  </div class="pos"
-          <div class="pos">
-            <h4>Samsung S7 Edge</h4>
-            <div class="pos">
-                <label for="precio">Precio 
-			<input name="precio" placeholder="$450" readonly="readonly"/>
-		</label><br></br>
-                <label for="stock">Stock 
-			<input name="stock" placeholder="45" readonly="readonly"/>
-		</label><br></br>
-                <label for="caracteristicas">Caracteristicas </label><br></br>		
-                <input type="caracteristica1" name="caracteristica1" placeholder="Pantalla 5.5 Pulgadas 2K Resolucion 2440 x 1860 pixels" size="50" readonly="readonly"/><br></br>
-		<input type="caracteristica2" name="caracteristica2" placeholder="Android 7.0 Nougat" size="50" readonly="readonly"/><br></br>
-		<input type="caracteristica3" name="caracteristica3" placeholder="Procesador Exynos 8890 Octa 2.3GHz" size="50" readonly="readonly"/><br></br>
-		<input type="caracteristica4" name="caracteristica4" placeholder="3GB de Ram & 32 GB memoria interna" size="50" readonly="readonly"/><br></br>
-	     </div class="pos">
-		<div class="pos">
-            	<input class="btn btn-primary" name="anadir" type="submit" value="Anadir a Carrito de compras" /> </div class="pos">
-          </div>
-  </div>
-</div>
-
-</body>
-</html>
-
+<script languaje="JavaScript">
+function pulsar() {
+alert("Se ha añadido al carrito de compras");
+}
+</script>
+<script type="text/javascript" src="<?php echo base_url("public/js/jQuery-3.2.1.min.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("public/js/bootstrap.js"); ?>"></script>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
